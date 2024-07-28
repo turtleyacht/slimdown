b2938a4f5f54595122055793884002c52f394aea
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-07-28 11:51:36 -0500

    Add more notes on interpreter behavior and specs
    A file can be treated as a module or invoked as a script from `python'
    based on the use of `-m' flag _without_ .py extension, or called as the
    path to the file, respectively.
    
    Updated the spec with a couple more section test cases. Deferring to
    external tools for publish and search solves two issues:
    
      * We don't need subsections, just a long run of text with occasional
        breaks.
      * A search tool could provide in-context results when looking for text
        Like `apropos' or `man -k' but paragraph-aware.
    
    It's still flexible if folks want to have a book be
    
      One long file with sections as chapters, index, and appendices.
    
      Or each file is a chapter with sections as subsections.
    
      Or each folder is a chapter and supporting files (graphics, LaTeX, and
      others) are woven together (by the separate _publishing_ tool).

---
+++ read_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import sys
+
+with open("SPEC", "r") as f:
+    for line in f:
+        print("a line was read.")
+        #send(recv_obj, line)
---
+++ simple_module.py
@@ -0,0 +1,21 @@
+##!/usr/bin/env python
+
+# Is this a module or a script?
+
+import sys
+
+print("Value of sys.argv[0] is:")
+print(sys.argv[0])
+
+print("Is my __name__ '__main__'?")
+print(__name__ == "__main__") # surprising
+print("Is my __name__ 'simple_module'?")
+print(__name__ == "simple_module")
+
+# sys.argv[0] is empty when invoked with stdin or interpreter with no args
+if (sys.argv[0] == "simple_module.py"):
+    print("Invoked as a script")
+elif (sys.argv[0] == "-c"):
+    print("Implicitly invoked with -c. May need to exclude '.py' from -m arg?")
+elif (sys.argv[0] != "-c"):
+    print("simple_module.py considered a module (full name printed above)")
