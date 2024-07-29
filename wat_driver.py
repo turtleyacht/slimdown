2e226b6c7ca5f605a60958d56e82bcda03c6d047
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-07-28 19:04:13 -0500

    Call multi-line print with parentheses

--- wat_driver.py
+++ wat_driver.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) < 2:
-        print """\
+        print("""\
 Usage: python wat_driver.py FILE\
-"""
+""")
     else:
         file = sys.argv[1]
         parser.read_lines.open_and_read(file, parser.handler)
