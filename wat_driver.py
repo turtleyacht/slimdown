6cf36a5ab06da63e76f463c0b82400e91460614c
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-07-28 19:18:20 -0500

    Moving handler to handlers/ package

--- wat_driver.py
+++ wat_driver.py
@@ -1,6 +1,6 @@
 # wat_driver.py
 
-import parser.handler
+import parser.handlers.handler
 import parser.read_lines
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ Usage: python wat_driver.py FILE\
         )
     else:
         file = sys.argv[1]
-        parser.read_lines.open_and_read(file, parser.handler)
+        parser.read_lines.open_and_read(file, parser.handlers.handler)
 else:
     print("I was invoked as a module. What do I do?")
     exit(1)
