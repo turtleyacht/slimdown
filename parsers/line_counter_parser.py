62c7bbe8022af0585152a258d12da838b4fa2c74
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-07-30 19:58:43 -0500

    Implement a line-counting parser w/ sanity handler
    So, two things:
    
      New parser! It's "smarter" because it stores state and passes it on to
      the handler.
    
      Backward-incompatible sanity handler :( Because it takes in new params
      line count and processed line count.
    
    This exactly reflects M.A. Jackson's admonishment not to use plain for
    loops, an indicator of a poor understanding of the structure of the data
    Fatal to a parser project -_-
    
    Given the shortcomings, this is a demonstration of a parser that can
    pass things to a handler.
    
    A generic thing would be nice. Interface? Structs?

---
+++ parsers/line_counter_parser.py
@@ -0,0 +1,13 @@
+import sys
+
+# Handlers must have an `on_line' function that handles
+# the line "event" after it is read from the file.
+def open_and_read(file, handler):
+    line_count = 0
+    processed_line_count = 0
+    with open(file, "r") as fp:
+        for line in fp:
+            line_count = line_count + 1
+            handler.on_line(line, line_count, processed_line_count)
+            processed_line_count = processed_line_count + 1
+            # you don't print the last processed line count!
