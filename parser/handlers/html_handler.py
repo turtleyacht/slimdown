bf6ead1873208daeaef761d792207c58f10f4d9c
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-08-07 18:16:41 -0500

    Wire up HTML emitter with HTML handler for parser
    Trying out the HTML output. The title is interpreted as a command. I'm
    leaning toward the spec no longer having titles.
    
    The filename can be the title. Then the file opens with the first
    section. No need to waste repeating that, like files that have the
    filename up top:
    
        # wat_driver.py
        # This script does ...
    
    Post-processing tools could easily prepend a title. Manual pages will
    have one most of the time. Multi-page documents may want templating with
    page numbers in the upper corners. In short, we expect processing to
    happen; slimdown no longer has to support titles.

---
+++ parser/handlers/html_handler.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../emitters")
+
+import html_emitter as emitter
+
+
+def on_line(line, info):
+    line_count = info["line_count"]
+    processed_line_count = info["processed_line_count"]
+    emitter.emit(line)
